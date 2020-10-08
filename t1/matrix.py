bd869560420bb3da087ead1b6fdaeec575fbf362
Zishuo Xu <z463xu@uwaterloo.ca>
2020-10-08 13:16:18 +0800

    add matrix_mul

--- t1/matrix.py
+++ t1/matrix.py
@@ -1,14 +1,14 @@
 class Matrix:
-    def __init__(self, row=0, col=0, fill=0.0):
+    def __init__(self, row=0, col=0, fill=0):
         """
         for question a)
         row: row number
-        column: column number
+        col: column number
         values is a 2-D list
         """
         self.row = row
-        self.column = col
-        self.values = [[fill] * self.column for i in range(row)]
+        self.col = col
+        self.values = [[fill] * self.col for i in range(row)]
 
     def get_row_number(self):
         """
@@ -16,21 +16,21 @@ class Matrix:
         """
         return self.row
 
-    def get_column_number(self):
+    def get_col_number(self):
         """
         for question a)
         """
-        return self.column
+        return self.col
 
-    def get_item(self, row, column):
+    def get_item(self, row, col):
         """
         for question a)
         """
         if row < 0 or row >= self.row:
             raise Exception("row number is out of range!")
-        if column < 0 or column >= self.column:
+        if col < 0 or col >= self.col:
             raise Exception("row number is out of range!")
-        return self.values[row][column]
+        return self.values[row][col]
 
 
 def print_matrix(matrix):
@@ -44,6 +44,19 @@ def print_matrix(matrix):
             s = s + str(value) + ', '
         s = s + str(row_list[-1]) + " |"
         print(s)
+
+
+def matrix_mul(A, B):
+    if A.col != B.row:
+        raise Exception("multiplication between A and B is not possible")
+    res = Matrix(A.row, B.col)
+    for i in range(0, res.row):
+        for j in range(0, res.col):
+            res.values[i][j] = 0
+            for idx in range(0, A.col):
+                res.values[i][j] += A.values[i][idx] * B.values[idx][j]
+    return res
+
 
 
 
