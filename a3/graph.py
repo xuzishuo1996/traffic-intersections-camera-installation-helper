5154ac9d69d6c5479c6af1ef4777bf5356ed2f18
Zishuo Xu <z463xu@uwaterloo.ca>
2020-10-30 13:59:47 +0800

    refactor a1's output to match a2's input

--- a3/graph.py
+++ a3/graph.py
@@ -3,7 +3,8 @@ from intersect import pp
 
 class Graph:
     def __init__(self):
-        self.vertices = {}  # dict(3-level) (key: str(street), val: {key: seg-ref, val: {key: Point, val: int [point-ref]}} )
+        # dict(3-level) (key: str(street), val: {key: seg-ref, val: {key: Point, val: int [point-ref]}} )
+        self.vertices = {}
         self.edges = set()     # set of Segments
         # self.edges = []     # list of Segments
 
@@ -15,7 +16,8 @@ class Graph:
             for seg_points in street_points[1].items():
                 print("    == seg ref: " + str(seg_points[0]) + " ==")
                 for point_ref_pair in seg_points[1].items():
-                    print("        " + str(point_ref_pair[1]) + ": " + pp(point_ref_pair[0]))
+                    print(
+                        "        " + str(point_ref_pair[1]) + ": " + pp(point_ref_pair[0]))
         print("}")
 
     def gen_output_vertices_dict(self):
@@ -27,25 +29,27 @@ class Graph:
         return vertices_dict
 
     def output(self):   # or __str__, __repr__ ?
-        print("V = {")
         output_vertices_dict = self.gen_output_vertices_dict()
-        for point in output_vertices_dict:
-            print("  " + str(output_vertices_dict[point]) + ": " + "(" + pp(point.x) + "," + pp(point.y) + ")")
-        print("}")
-
-        # for test only
-        # print("E = {")
-        # for item in self.edges:
-        #     print("  <" + str(item.point1) + "," + str(item.point2) + ">")
+        # print("V = {")
+        # for point in output_vertices_dict:
+        #     print("  " + str(output_vertices_dict[point]) +
+        #           ": " + "(" + pp(point.x) + "," + pp(point.y) + ")")
         # print("}")
+        num = len(output_vertices_dict)
+        if num > 0:
+            s = "V "
+            s += str(num)
+            print(s)
 
-        print("E = {")
         edges_list = list(self.edges)
         if len(edges_list) != 0:
-            for item in edges_list[:-1]:
-                print("  <" + str(output_vertices_dict[item.point1]) + "," + str(output_vertices_dict[item.point2]) + ">,")
-            print("  <" + str(output_vertices_dict[edges_list[-1].point1]) + "," + str(output_vertices_dict[edges_list[-1].point2]) + ">")
-        print("}")
+            s = "E {"
+            for item in edges_list:
+                s += "<" + str(output_vertices_dict[item.point1]) + "," + str(
+                    output_vertices_dict[item.point2]) + ">,"
+            s = s.rstrip(',')
+            s += "}"
+            print(s)
 
 
 # # for test only - test graph.output()
