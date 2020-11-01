381c95a29cb0cc9d93fdae15ef522de30c79e0d5
Zishuo Xu <z463xu@uwaterloo.ca>
2020-11-01 15:45:25 +0800

    add judging intersection for non-adjacent. need to add for adjacent segs

--- a3/SegUtility.cpp
+++ a3/SegUtility.cpp
@@ -14,14 +14,32 @@ bool three_points_on_same_line(Point p1, Point p2, Point p3)
     return cross_product(p2 - p1, p3 - p1) == 0;
 }
 
+// Segment is small, so pass-by-value
 bool segs_on_same_line(const Segment s1, const Segment s2)
 {
     return three_points_on_same_line(s1.p1, s1.p2, s2.p1) && three_points_on_same_line(s1.p1, s1.p2, s2.p2);
 }
 
+/* use it only when segs are on the same line */
 bool same_line_intersects(const Segment s1, const Segment s2)
 {
     return (std::min(s1.p1.x, s1.p2.x) <= std::max(s2.p1.x, s2.p2.x)) && (std::max(s1.p1.x, s1.p2.x) >= std::min(s2.p1.x, s2.p2.x)) && (std::min(s1.p1.y, s1.p2.y) <= std::max(s2.p1.y, s2.p2.y)) && (std::max(s1.p1.y, s1.p2.y) >= std::min(s2.p1.y, s2.p2.y));
+}
+
+bool straddle(const Segment s1, const Segment s2)
+{
+    Point p0 = Point(s1.p2) - Point(s1.p1);
+    Point p1 = Point(s2.p1) - Point(s1.p1);
+    Point p2 = Point(s2.p2) - Point(s1.p1);
+    return cross_product(p1, p0) * cross_product(p2, p0) <= 0;
+}
+
+/* use it only when segs are not on the same line. 
+   is_intersected algorithm reference: https://zhuanlan.zhihu.com/p/37360022
+   judge if both segment cross the other line using cross product. */
+bool is_intersected(const Segment s1, const Segment s2)
+{
+    return straddle(s1, s2) && straddle(s2, s1);
 }
 
 int main()
@@ -43,4 +61,18 @@ int main()
     std::cout << segs_on_same_line(s1, s2) << std::endl;    // 0
     std::cout << same_line_intersects(s1, s3) << std::endl; // 1
     std::cout << same_line_intersects(s1, s4) << std::endl; // 0
+    std::cout << same_line_intersects(s3, s4) << std::endl; // 1
+
+    Segment vertical0 = Segment(Point(0, 0), Point(0, 2));
+    Segment vertical1 = Segment(Point(1, 0), Point(1, 2));
+    Segment horizontal1 = Segment(Point(0, 1), Point(1, 1));
+    Segment horizontal2 = Segment(Point(0, 2), Point(1, 2));
+    std::cout << segs_on_same_line(vertical0, vertical1) << std::endl; // 0
+
+    std::cout << is_intersected(s1, s2) << std::endl;                 // 1
+    std::cout << is_intersected(vertical0, vertical1) << std::endl;   // 0
+    std::cout << is_intersected(vertical0, horizontal1) << std::endl; // 1
+    std::cout << is_intersected(vertical0, horizontal2) << std::endl; // 1
+    Segment unrelated = Segment(Point(100, 0), Point(110, 1));
+    std::cout << is_intersected(vertical0, unrelated) << std::endl; // 0
 }
